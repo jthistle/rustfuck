2d33c68f3b096ee7b33c38cce909c6d48e1f8300
James Thistlewood <jamesthistlewood@gmail.com>
2021-04-17 18:56:47 +0100

    add move optimization

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ enum TokenType {
     LoopStart,
     LoopEnd,
     Set,
+    Move,
 }
 
 #[derive(Debug)]
@@ -36,6 +37,7 @@ impl Token {
 
 type Ast = Vec<Token>;
 
+/// Parses raw text into an intermediate representation.
 fn parse(raw: &String) -> Result<Ast, &'static str> {
     let mut ast = Ast::new();
     let mut chars = raw.chars();
@@ -100,8 +102,13 @@ fn parse(raw: &String) -> Result<Ast, &'static str> {
     }
 }
 
+/// A vector of replacements to be made with form:
+/// `(begin, end, replacement)`
+/// where `begin` is included and `end` is excluded.
 type ReplaceVec = Vec::<(usize, usize, Token)>;
 
+/// Given a sorted vector of replacements to be made, replace the given
+/// ranges of tokens with a single other token in the AST.
 fn replace_in_ast(ast: &mut Ast, mut replacements: ReplaceVec) {
     replacements.reverse();
     for (start, end, token) in replacements {
@@ -110,6 +117,10 @@ fn replace_in_ast(ast: &mut Ast, mut replacements: ReplaceVec) {
     }
 }
 
+/// Collapses duplicated tokens into a single token.
+/// 
+/// e.g. `------`, which is represented as six `TokenType::Sub` with value `1`,
+/// is replaced by a single `TokenType::Sub` with value `6`. This applies to `-`, `+`, `>`, and `<`.
 fn pass_collapse_duplicated(ast: &mut Ast) {
     let mut start: usize = 0;
     let mut count: usize = 0;
@@ -140,6 +151,12 @@ fn pass_collapse_duplicated(ast: &mut Ast) {
     replace_in_ast(ast, replace);
 }
 
+/// Replaces 'zeroing' instructions with a single token to reduce time spent in loops.
+/// 
+/// This replaces `[-]` and `[+]` (and all variants of these which have an odd number of inner symbols)
+/// with a single token of `TokenType::Set` and value `0`.
+/// 
+/// This pass must be run after Collapse Duplicated.
 fn pass_zero_cell(ast: &mut Ast) {
     let mut replace = ReplaceVec::new();
     let mut progress = 0;
@@ -161,11 +178,56 @@ fn pass_zero_cell(ast: &mut Ast) {
     replace_in_ast(ast, replace);
 }
 
+/// Replaces idiomatic moves of the form `[->+<]` with a single `TokenType::Move` token.
+/// This also accepts any number of left/right tokens, e.g. `[->>>+<<<]` can also be optimized.
+/// Left/right tokens can also be put in the opposite order, e.g. `[-<<<+>>>]`.1
+/// 
+/// This pass must be run after Collapse Duplicated.
+fn pass_move_value(ast: &mut Ast) {
+    let mut replace = ReplaceVec::new();
+    let mut next_direction = TokenType::Invalid;
+    let mut move_count = -1;
+    let mut progress = 0;
+
+    for (i, node) in ast.iter().enumerate() {
+        if progress == 0 && node.tk == TokenType::LoopStart {
+            progress += 1;
+        } else if progress == 1 && node.tk == TokenType::Sub && node.value == 1 {
+            progress += 1;
+        } else if progress == 2 && (node.tk == TokenType::Left || node.tk == TokenType::Right) && node.value == 1 {
+            if node.tk == TokenType::Left {
+                next_direction = TokenType::Right;
+            } else {
+                next_direction = TokenType::Left;
+            }
+            move_count = node.value;
+            progress += 1;
+        } else if progress == 3 && node.tk == TokenType::Add && node.value == 1 {
+            progress += 1;
+        } else if progress == 4 && node.tk == next_direction && node.value == move_count {
+            progress += 1;
+        } else if progress == 5 && node.tk == TokenType::LoopEnd {
+            replace.push((i - 5, i + 1, Token::new(
+                TokenType::Move, move_count * (if next_direction == TokenType::Right { -1 } else { 1 })
+            )));
+            progress = 0;
+        } else {
+            progress = 0;
+        }
+    }
+
+    replace_in_ast(ast, replace);
+}
+
+/// Runs optimizer passes on the AST.
 fn optimize(ast: &mut Ast) {
     pass_collapse_duplicated(ast);
     pass_zero_cell(ast);
+    pass_move_value(ast);
 }
 
+/// Caches loop jump endpoints to reduce time spent searching during 
+/// execution.
 fn link_loops(ast: &mut Ast) -> Result<(), &'static str> {
     let mut loop_stack: Vec<usize> = Vec::new();
 
@@ -197,6 +259,7 @@ fn link_loops(ast: &mut Ast) -> Result<(), &'static str> {
 type CellMaxSize = u64;
 type CellMaxSizeLower = u32;
 
+/// Runs the AST.
 fn execute(ast: &Ast, cell_size: u16, tape_size: usize) -> Result<(), &'static str> 
 {
     if cell_size < 1 || cell_size > 32 {
@@ -233,14 +296,14 @@ fn execute(ast: &Ast, cell_size: u16, tape_size: usize) -> Result<(), &'static s
             },
             TokenType::Left => {
                 if data_pointer < token.value as usize {
-                    return Err("Data pointer moved out of bounds (too far left)!")
+                    return Err("Data pointer moved out of bounds (too far left)")
                 }
                 data_pointer -= token.value as usize;
             },
             TokenType::Right => {
                 let new_pos = data_pointer + token.value as usize;
                 if new_pos >= tape_size {
-                    return Err("Data pointer moved out of bounds (too far right)!")
+                    return Err("Data pointer moved out of bounds (too far right)")
                 } else if new_pos > cells.len() {
                     // Allocate more space for the tape, we need it
                     cells.extend(
@@ -291,6 +354,26 @@ fn execute(ast: &Ast, cell_size: u16, tape_size: usize) -> Result<(), &'static s
             TokenType::Set => {
                 cells[data_pointer] = token.value as CellMaxSize;
             },
+            TokenType::Move => {
+                let dest = data_pointer as i32 + token.value;
+                if dest < 0 {
+                    return Err("Data pointer moved out of bounds (too far left)")
+                }
+                
+                let dest = dest as usize;
+                if dest >= tape_size {
+                    return Err("Data pointer moved out of bounds (too far right)")
+                } else if dest > cells.len() {
+                    // Allocate more space for the tape, we need it
+                    // TODO this is duplicated code, refactor this in future
+                    cells.extend(
+                        iter::repeat::<CellMaxSize>(0).take(dest - cells.len() + 1000)
+                    );
+                }
+
+                cells[dest] = cells[data_pointer];
+                cells[data_pointer] = 0;
+            },
             TokenType::End => return Ok(()),
             _ => {},
         }
@@ -327,6 +410,8 @@ fn main() -> Result<(), &'static str> {
             Ok(x) => x,
             Err(_) => return Err("Could not open file"),
         }
+    } else if raw == "" {
+        return Err("Please provide a filename. Use flag  --help  for usage help.")
     }
 
     let mut ast = match parse(&raw) {
@@ -334,9 +419,13 @@ fn main() -> Result<(), &'static str> {
         Err(err) => return Err(err),
     };
 
+    println!("before: {:?}", ast);
+
     if do_optimize {
         optimize(&mut ast);       
     }
+
+    println!("after: {:?}", ast);
 
     match link_loops(&mut ast) {
         Ok(_) => {},
